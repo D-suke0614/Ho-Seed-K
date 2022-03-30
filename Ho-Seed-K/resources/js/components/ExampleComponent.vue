ddf640e0b4f1db345b67d834c398a910338f39c7
Taisei Namekawa <s.namety.0106.hta1@gmail.com>
2022-03-30 21:05:57 +0900

    Revert "first commit"
    This reverts commit 5b8bb46a5f829babba4d185cc67d5d212384ec33.

--- Ho-Seed-K/resources/js/components/ExampleComponent.vue
+++ Ho-Seed-K/resources/js/components/ExampleComponent.vue
@@ -1,57 +1,23 @@
 <template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card">
-          <div class="card-header">Laravel Vue.js</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
 
-          <div class="card-body">I'm challenger</div>
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
         </div>
-
-
-<div id="contents">
-    <div class="tab_box">
-      <ul class="tab_list">
-        <li v-on:click="change('A')" v-bind:class="{'active': isActive === 'A'}">タブ1</li>
-        <li v-on:click="change('B')" v-bind:class="{'active': isActive === 'B'}">タブ2</li>
-        <li v-on:click="change('C')" v-bind:class="{'active': isActive === 'C'}">タブ3</li>
-      </ul>
- 
-      <ul class="article">
-        <li v-if="isActive === 'A'">ダミーテキスト1</li>
-        <li v-else-if="isActive === 'B'">ダミーテキスト2</li>
-        <li v-else-if="isActive === 'C'">ダミーテキスト3</li>
-      </ul>
     </div>
- 
-  </div>
-
-
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
-export default {
-  mounted() {
-    console.log("Component mounted.");
-  },
-};
-
-// new Vue({
-//   el: '#contents',
-//   data: {
-//     isActive: '1'
-//   },
-//   methods: {
-//     change: function(num){
-//       this.isActive = num
-//     }
-//   }
-// })
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
 </script>
-
-<style lang="" scoped>
- 
-</style>
